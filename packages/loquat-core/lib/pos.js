4058c00df2860fc208042160658fb93636aae08d
Susisu <susisu2413@yahoo.co.jp>
2016-09-05 10:30:05 +0900

    [core] Add SourcePos class
    TODO: consider surrogate pairs?

---
+++ packages/loquat-core/lib/pos.js
@@ -0,0 +1,104 @@
+"use strict";
+
+function end() {
+    module.exports = Object.freeze({
+        SourcePos
+    });
+}
+
+const DEFAULT_TAB_WIDTH = 8;
+
+class SourcePos {
+    constructor(name, line, column) {
+        this.name   = name;
+        this.line   = line;
+        this.column = column;
+    }
+
+    static init(name) {
+        return new SourcePos(name, 1, 1);
+    }
+
+    static equal(posA, posB) {
+        return posA.name   === posB.name
+            && posA.line   === posB.line
+            && posA.column === posB.column;
+    }
+
+    static compare(posA, posB) {
+        return posA.name   < posB.name   ? -1
+             : posA.name   > posB.name   ? 1
+             : posA.line   < posB.line   ? -1
+             : posA.line   > posB.line   ? 1
+             : posA.column < posB.column ? -1
+             : posA.column > posB.column ? 1
+                                         : 0;
+    }
+
+    toString() {
+        return (this.name === "" ? "" : `"${this.name}"`)
+            + `(line ${this.line.toString()}, column ${this.column.toString()})`;
+    }
+
+    clone() {
+        return new SourcePos(this.name, this.line, this.column);
+    }
+
+    setName(name) {
+        return new SourcePos(name, this.line, this.column);
+    }
+
+    setLine(line) {
+        return new SourcePos(this.name, line, this.column);
+    }
+
+    setColumn(column) {
+        return new SourcePos(this.name, this.line, column);
+    }
+
+    addChar(char, tabWidth) {
+        tabWidth = tabWidth | 0;
+        if (tabWidth <= 0) {
+            tabWidth = DEFAULT_TAB_WIDTH;
+        }
+        let copy = this.clone();
+        switch (char) {
+        case "\n":
+            copy.line  += 1;
+            copy.column = 1;
+            break;
+        case "\t":
+            copy.column += tabWidth - (copy.column - 1) % tabWidth;
+            break;
+        default:
+            copy.column += 1;
+        }
+        return copy;
+    }
+
+    addString(str, tabWidth) {
+        tabWidth = tabWidth | 0;
+        if (tabWidth <= 0) {
+            tabWidth = DEFAULT_TAB_WIDTH;
+        }
+        let copy = this.clone();
+        // TODO: consider surrogate pairs?
+        let len = str.length;
+        for (let i = 0; i < len; i++) {
+            switch (str[i]) {
+            case "\n":
+                copy.line  += 1;
+                copy.column = 1;
+                break;
+            case "\t":
+                copy.column += tabWidth - (copy.column - 1) % tabWidth;
+                break;
+            default:
+                copy.column += 1;
+            }
+        }
+        return copy;
+    }
+}
+
+end();
