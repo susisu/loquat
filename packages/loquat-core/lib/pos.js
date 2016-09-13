f8c320b83de05e54b12d6c3f711eac83ccb7f6cc
Susisu <susisu2413@yahoo.co.jp>
2016-09-13 11:34:42 +0900

    [core] Add docs for the pos module

--- packages/loquat-core/lib/pos.js
+++ packages/loquat-core/lib/pos.js
@@ -3,6 +3,10 @@
  * copyright (c) 2016 Susisu
  */
 
+/**
+ * @module pos
+ */
+
 "use strict";
 
 function end() {
@@ -13,23 +17,52 @@ function end() {
 
 const DEFAULT_TAB_WIDTH = 8;
 
+/**
+ * An instance of the `SourcePos` class represents a specific position in the source.
+ * @static
+ */
 class SourcePos {
+    /**
+     * Creates a new `SourcePos` instance.
+     * @param {string} name Name of the source.
+     * @param {number} line Line in the source.
+     * @parma {number} coulmn Column in the source.
+     */
     constructor(name, line, column) {
         this.name   = name;
         this.line   = line;
         this.column = column;
     }
 
+    /**
+     * Creates a new `SourcePos` instance initialized with `line = 1` and `column = 1`.
+     * @param {string} name Name of the source.
+     * @returns {SourcePos} New `SourcePos` instance.
+     */
     static init(name) {
         return new SourcePos(name, 1, 1);
     }
 
+    /**
+     * Checks if two `SourcePos` instances describe the same position.
+     * @param {SourcePos} posA
+     * @param {SourcePos} posB
+     * @returns {boolean} `true` if two `SoucePos` instances describe the same position.
+     */
     static equal(posA, posB) {
         return posA.name   === posB.name
             && posA.line   === posB.line
             && posA.column === posB.column;
     }
 
+    /**
+     * Compares two `SourcePos` instances.
+     * @param {SourcePos} posA
+     * @parma {SourcePos} posB
+     * @returns {number} Negative if `posA` describes a position ahead of `posB.
+     * Positive if `posA` describes a position behind `posB`.
+     * Zero if `posA` and `posB` describe the same.
+     */
     static compare(posA, posB) {
         return posA.name   < posB.name   ? -1
              : posA.name   > posB.name   ? 1
@@ -40,27 +73,55 @@ class SourcePos {
                                          : 0;
     }
 
+    /**
+     * Returns the string representation of the position.
+     * @returns {string} The string representation of the position.
+     */
     toString() {
         return (this.name === "" ? "" : `"${this.name}"`)
             + `(line ${this.line.toString()}, column ${this.column.toString()})`;
     }
 
+    /**
+     * Creates a new copy of the instance.
+     * @returns {SourcePos} Copy of the instnace.
+     */
     clone() {
         return new SourcePos(this.name, this.line, this.column);
     }
 
+    /**
+     * Creates a new copy of the instance with `name` set to the specified value.
+     * @param {string} name
+     * @returns {SourcePos} Copy of the instance.
+     */
     setName(name) {
         return new SourcePos(name, this.line, this.column);
     }
 
+    /**
+     * Creates a new copy of the instance with `line` set to the specified value.
+     * @param {number} line
+     * @returns {SourcePos} Copy of the instance.
+     */
     setLine(line) {
         return new SourcePos(this.name, line, this.column);
     }
 
+    /**
+     * Creates a new copy of the instance with `column` set to the specified value.
+     * @param {number} column
+     * @returns {SourcePos} Copy of the instance.
+     */
     setColumn(column) {
         return new SourcePos(this.name, this.line, column);
     }
 
+    /**
+     * @param {string} char
+     * @param {number} tabWidth
+     * @returns {SourcePos}
+     */
     addChar(char, tabWidth) {
         tabWidth = tabWidth | 0;
         if (tabWidth <= 0) {
@@ -83,6 +144,13 @@ class SourcePos {
         return copy;
     }
 
+    /**
+     * @param {string} str
+     * @param {number} tabWidth
+     * @param {boolean} useCodePoint If `true` specified, the characters in the `str` are counted
+     * based on the UTF-16 code point.
+     * @returns {SourcePos}
+     */
     addString(str, tabWidth, useCodePoint) {
         tabWidth = tabWidth | 0;
         if (tabWidth <= 0) {
