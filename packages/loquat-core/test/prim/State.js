45d52ab0f4406c01f167c4e2cdf1d310ce29a43f
Susisu <susisu2413@yahoo.co.jp>
2016-09-17 16:52:20 +0900

    [core] Add test for `State#setInput()`

--- packages/loquat-core/test/prim/State.js
+++ packages/loquat-core/test/prim/State.js
@@ -8,4 +8,5 @@
 describe(".State", () => {
     require("./State/constructor.js");
     require("./State/equal.js");
+    require("./State/setInput.js");
 });
---
+++ packages/loquat-core/test/prim/State/setInput.js
@@ -0,0 +1,33 @@
+/*
+ * loquat-core test / prim.State#setInput()
+ * copyright (c) 2016 Susisu
+ */
+
+"use strict";
+
+const { expect } = require("chai");
+
+const { SourcePos } = require("pos.js");
+const { Config, State } = require("prim.js");
+
+describe("#setInput(input)", () => {
+    it("should a copy of the state, with specified `input'", () => {
+        let state = new State(
+            new Config({ tabWidth: 4, useCodePoint: true }),
+            "foo",
+            new SourcePos("nyancat", 496, 28),
+            "none"
+        );
+        let copy = state.setInput("bar");
+        expect(copy).not.to.equal(state);
+        expect(State.equal(
+            copy,
+            new State(
+                new Config({ tabWidth: 4, useCodePoint: true }),
+                "bar",
+                new SourcePos("nyancat", 496, 28),
+                "none"
+            )
+        )).to.be.true;
+    });
+});
