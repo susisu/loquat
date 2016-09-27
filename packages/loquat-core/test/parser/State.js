f73e5767be470b8374f4f0c4dd95eb9c6dccaf5e
Susisu <susisu2413@yahoo.co.jp>
2016-09-27 11:35:42 +0900

    [core] Add test for `State#setConfig()`

--- packages/loquat-core/test/parser/State.js
+++ packages/loquat-core/test/parser/State.js
@@ -8,6 +8,7 @@
 describe(".State", () => {
     require("./State/constructor.js");
     require("./State/equal.js");
+    require("./State/setConfig.js");
     require("./State/setInput.js");
     require("./State/setPosition.js");
     require("./State/setUserState.js");
---
+++ packages/loquat-core/test/parser/State/setConfig.js
@@ -0,0 +1,33 @@
+/*
+ * loquat test / parser.State#setConfig()
+ * copyright (c) 2016 Susisu
+ */
+
+"use strict";
+
+const { expect } = require("chai");
+
+const { SourcePos } = require("pos.js");
+const { Config, State } = require("parser.js");
+
+describe("#setConfig(config)", () => {
+    it("should a copy of the state, with specified `config'", () => {
+        let state = new State(
+            new Config({ tabWidth: 4, useCodePoint: true }),
+            "foo",
+            new SourcePos("nyancat", 496, 28),
+            "none"
+        );
+        let copy = state.setConfig(new Config({ tabWidth: 8, useCodePoint: false }));
+        expect(copy).not.to.equal(state);
+        expect(State.equal(
+            copy,
+            new State(
+                new Config({ tabWidth: 8, useCodePoint: false }),
+                "foo",
+                new SourcePos("nyancat", 496, 28),
+                "none"
+            )
+        )).to.be.true;
+    });
+});
