1044464b73e45197a1bdfd3dd2a0087e1db9d1fb
Susisu <susisu2413@yahoo.co.jp>
2016-09-28 11:06:44 +0900

    [core] Add index

--- packages/loquat-core/index.js
+++ packages/loquat-core/index.js
@@ -5,4 +5,52 @@
 
 "use strict";
 
-module.exports = Object.freeze({});
+const { show } = require("./lib/utils.js");
+const { SourcePos } = require("./lib/pos.js");
+const {
+    ErrorMessageType,
+    ErrorMessage,
+    AbstractParseError,
+    ParseError,
+    LazyParseError
+} = require("./lib/error.js");
+const { uncons } = require("./lib/stream.js");
+const {
+    Config,
+    State,
+    Result,
+    AbstractParser,
+    Parser,
+    LazyParser,
+    lazy,
+    parse,
+    isParser,
+    assertParser,
+    extendParser
+} = require("./lib/parser.js");
+
+module.exports = Object.freeze({
+    show,
+
+    SourcePos,
+
+    ErrorMessageType,
+    ErrorMessage,
+    AbstractParseError,
+    ParseError,
+    LazyParseError,
+
+    uncons,
+
+    Config,
+    State,
+    Result,
+    AbstractParser,
+    Parser,
+    LazyParser,
+    lazy,
+    parse,
+    isParser,
+    assertParser,
+    extendParser
+});
