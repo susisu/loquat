c9b15ba307d6fea42e99312a2f2aa66046c80816
Susisu <susisu2413@yahoo.co.jp>
2016-10-11 00:56:37 +0900

    [core] Simplify index

--- packages/loquat-core/index.js
+++ packages/loquat-core/index.js
@@ -5,57 +5,34 @@
 
 "use strict";
 
-const _utils = require("./lib/utils.js");
-const show = _utils.show;
-
-const _pos = require("./lib/pos.js");
-const SourcePos = _pos.SourcePos;
-
-const _error = require("./lib/error.js");
-const ErrorMessageType   = _error.ErrorMessageType;
-const ErrorMessage       = _error.ErrorMessage;
-const AbstractParseError = _error.AbstractParseError;
-const ParseError         = _error.ParseError;
-const LazyParseError     = _error.LazyParseError;
-
+const _utils  = require("./lib/utils.js");
+const _pos    = require("./lib/pos.js");
+const _error  = require("./lib/error.js");
 const _stream = require("./lib/stream.js");
-const uncons = _stream.uncons;
-
 const _parser = require("./lib/parser.js");
-const Config         = _parser.Config;
-const State          = _parser.State;
-const Result         = _parser.Result;
-const AbstractParser = _parser.AbstractParser;
-const Parser         = _parser.Parser;
-const LazyParser     = _parser.LazyParser;
-const lazy           = _parser.lazy;
-const parse          = _parser.parse;
-const isParser       = _parser.isParser;
-const assertParser   = _parser.assertParser;
-const extendParser   = _parser.extendParser;
 
 module.exports = Object.freeze({
-    show,
+    show: _utils.show,
 
-    SourcePos,
+    SourcePos: _pos.SourcePos,
 
-    ErrorMessageType,
-    ErrorMessage,
-    AbstractParseError,
-    ParseError,
-    LazyParseError,
+    ErrorMessageType  : _error.ErrorMessageType,
+    ErrorMessage      : _error.ErrorMessage,
+    AbstractParseError: _error.AbstractParseError,
+    ParseError        : _error.ParseError,
+    LazyParseError    : _error.LazyParseError,
 
-    uncons,
+    uncons: _stream.uncons,
 
-    Config,
-    State,
-    Result,
-    AbstractParser,
-    Parser,
-    LazyParser,
-    lazy,
-    parse,
-    isParser,
-    assertParser,
-    extendParser
+    Config        : _parser.Config,
+    State         : _parser.State,
+    Result        : _parser.Result,
+    AbstractParser: _parser.AbstractParser,
+    Parser        : _parser.Parser,
+    LazyParser    : _parser.LazyParser,
+    lazy          : _parser.lazy,
+    parse         : _parser.parse,
+    isParser      : _parser.isParser,
+    assertParser  : _parser.assertParser,
+    extendParser  : _parser.extendParser
 });
