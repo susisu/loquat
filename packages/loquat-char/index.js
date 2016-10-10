62c57d41244bbc254d34888577881f3ad37b1efe
Susisu <susisu2413@yahoo.co.jp>
2016-10-11 01:03:10 +0900

    [char] Simplify index

--- packages/loquat-char/index.js
+++ packages/loquat-char/index.js
@@ -5,49 +5,29 @@
 
 "use strict";
 
-module.exports = (core, sugar) => {
-    let _char = require("./lib/char.js")(core);
-    const string     = _char.string;
-    const satisfy    = _char.satisfy;
-    const oneOf      = _char.oneOf;
-    const noneOf     = _char.noneOf;
-    const char       = _char.char;
-    const anyChar    = _char.anyChar;
-    const space      = _char.space;
-    const spaces     = _char.spaces;
-    const newline    = _char.newline;
-    const tab        = _char.tab;
-    const upper      = _char.upper;
-    const lower      = _char.lower;
-    const letter     = _char.letter;
-    const digit      = _char.digit;
-    const alphaNum   = _char.alphaNum;
-    const octDigit   = _char.octDigit;
-    const hexDigit   = _char.hexDigit;
-    const manyChars  = _char.manyChars;
-    const manyChars1 = _char.manyChars1;
-    const regexp     = _char.regexp;
+module.exports = (_core, sugar) => {
+    let _char = require("./lib/char.js")(_core);
 
     return Object.freeze({
-        string,
-        satisfy,
-        oneOf,
-        noneOf,
-        char,
-        anyChar,
-        space,
-        spaces,
-        newline,
-        tab,
-        upper,
-        lower,
-        letter,
-        digit,
-        alphaNum,
-        octDigit,
-        hexDigit,
-        manyChars,
-        manyChars1,
-        regexp
+        string    : _char.string,
+        satisfy   : _char.satisfy,
+        oneOf     : _char.oneOf,
+        noneOf    : _char.noneOf,
+        char      : _char.char,
+        anyChar   : _char.anyChar,
+        space     : _char.space,
+        spaces    : _char.spaces,
+        newline   : _char.newline,
+        tab       : _char.tab,
+        upper     : _char.upper,
+        lower     : _char.lower,
+        letter    : _char.letter,
+        digit     : _char.digit,
+        alphaNum  : _char.alphaNum,
+        octDigit  : _char.octDigit,
+        hexDigit  : _char.hexDigit,
+        manyChars : _char.manyChars,
+        manyChars1: _char.manyChars1,
+        regexp    : _char.regexp
     });
 };
