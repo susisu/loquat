f28eb0a1a4a6c735ede10390ab650a55e02c8c76
Susisu <susisu2413@yahoo.co.jp>
2016-10-10 17:31:50 +0900

    [prim] Simplify index

--- packages/loquat-prim/index.js
+++ packages/loquat-prim/index.js
@@ -5,73 +5,41 @@
 
 "use strict";
 
-module.exports = (core, sugar) => {
-    let _prim = require("./lib/prim.js")(core);
-    const map          = _prim.map;
-    const pure         = _prim.pure;
-    const ap           = _prim.ap;
-    const left         = _prim.left;
-    const right        = _prim.right;
-    const bind         = _prim.bind;
-    const then         = _prim.then;
-    const fail         = _prim.fail;
-    const mzero        = _prim.mzero;
-    const mplus        = _prim.mplus;
-    const label        = _prim.label;
-    const labels       = _prim.labels;
-    const unexpected   = _prim.unexpected;
-    const tryParse     = _prim.tryParse;
-    const lookAhead    = _prim.lookAhead;
-    const reduceMany   = _prim.reduceMany;
-    const many         = _prim.many;
-    const skipMany     = _prim.skipMany;
-    const tokens       = _prim.tokens;
-    const token        = _prim.token;
-    const tokenPrim    = _prim.tokenPrim;
-    const getState     = _prim.getState;
-    const setState     = _prim.setState;
-    const updateState  = _prim.updateState;
-    const getConfig    = _prim.getConfig;
-    const setConfig    = _prim.setConfig;
-    const getInput     = _prim.getInput;
-    const setInput     = _prim.setInput;
-    const getPosition  = _prim.getPosition;
-    const setPosition  = _prim.setPosition;
-    const getUserState = _prim.getUserState;
-    const setUserState = _prim.setUserState;
+module.exports = (_core, sugar) => {
+    const _prim = require("./lib/prim.js")(_core);
 
     return Object.freeze({
-        map,
-        pure,
-        ap,
-        left,
-        right,
-        bind,
-        then,
-        fail,
-        mzero,
-        mplus,
-        label,
-        labels,
-        unexpected,
-        tryParse,
-        lookAhead,
-        reduceMany,
-        many,
-        skipMany,
-        tokens,
-        token,
-        tokenPrim,
-        getState,
-        setState,
-        updateState,
-        getConfig,
-        setConfig,
-        getInput,
-        setInput,
-        getPosition,
-        setPosition,
-        getUserState,
-        setUserState
+        map         : _prim.map,
+        pure        : _prim.pure,
+        ap          : _prim.ap,
+        left        : _prim.left,
+        right       : _prim.right,
+        bind        : _prim.bind,
+        then        : _prim.then,
+        fail        : _prim.fail,
+        mzero       : _prim.mzero,
+        mplus       : _prim.mplus,
+        label       : _prim.label,
+        labels      : _prim.labels,
+        unexpected  : _prim.unexpected,
+        tryParse    : _prim.tryParse,
+        lookAhead   : _prim.lookAhead,
+        reduceMany  : _prim.reduceMany,
+        many        : _prim.many,
+        skipMany    : _prim.skipMany,
+        tokens      : _prim.tokens,
+        token       : _prim.token,
+        tokenPrim   : _prim.tokenPrim,
+        getState    : _prim.getState,
+        setState    : _prim.setState,
+        updateState : _prim.updateState,
+        getConfig   : _prim.getConfig,
+        setConfig   : _prim.setConfig,
+        getInput    : _prim.getInput,
+        setInput    : _prim.setInput,
+        getPosition : _prim.getPosition,
+        setPosition : _prim.setPosition,
+        getUserState: _prim.getUserState,
+        setUserState: _prim.setUserState
     });
 };
