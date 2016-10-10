54daf87af2ea336d07719fe2e455ed3c5c160f14
Susisu <susisu2413@yahoo.co.jp>
2016-10-11 01:04:31 +0900

    [combinators] Simplify index

--- packages/loquat-combinators/index.js
+++ packages/loquat-combinators/index.js
@@ -5,53 +5,31 @@
 
 "use strict";
 
-module.exports = (core, sugar) => {
-    let _combinators = require("./lib/combinators.js")(core);
-    const choice        = _combinators.choice;
-    const option        = _combinators.option;
-    const optionMaybe   = _combinators.optionMaybe;
-    const optional      = _combinators.optional;
-    const between       = _combinators.between;
-    const many1         = _combinators.many1;
-    const skipMany1     = _combinators.skipMany1;
-    const sepBy         = _combinators.sepBy;
-    const sepBy1        = _combinators.sepBy1;
-    const sepEndBy      = _combinators.sepEndBy;
-    const sepEndBy1     = _combinators.sepEndBy1;
-    const endBy         = _combinators.endBy;
-    const endBy1        = _combinators.endBy1;
-    const count         = _combinators.count;
-    const chainl        = _combinators.chainl;
-    const chainl1       = _combinators.chainl1;
-    const chainr        = _combinators.chainr;
-    const chainr1       = _combinators.chainr1;
-    const anyToken      = _combinators.anyToken;
-    const notFollowedBy = _combinators.notFollowedBy;
-    const eof           = _combinators.eof;
-    const manyTill      = _combinators.manyTill;
+module.exports = (_core, sugar) => {
+    let _combinators = require("./lib/combinators.js")(_core);
 
     return Object.freeze({
-        choice,
-        option,
-        optionMaybe,
-        optional,
-        between,
-        many1,
-        skipMany1,
-        sepBy,
-        sepBy1,
-        sepEndBy,
-        sepEndBy1,
-        endBy,
-        endBy1,
-        count,
-        chainl,
-        chainl1,
-        chainr,
-        chainr1,
-        anyToken,
-        notFollowedBy,
-        eof,
-        manyTill
+        choice       : _combinators.choice,
+        option       : _combinators.option,
+        optionMaybe  : _combinators.optionMaybe,
+        optional     : _combinators.optional,
+        between      : _combinators.between,
+        many1        : _combinators.many1,
+        skipMany1    : _combinators.skipMany1,
+        sepBy        : _combinators.sepBy,
+        sepBy1       : _combinators.sepBy1,
+        sepEndBy     : _combinators.sepEndBy,
+        sepEndBy1    : _combinators.sepEndBy1,
+        endBy        : _combinators.endBy,
+        endBy1       : _combinators.endBy1,
+        count        : _combinators.count,
+        chainl       : _combinators.chainl,
+        chainl1      : _combinators.chainl1,
+        chainr       : _combinators.chainr,
+        chainr1      : _combinators.chainr1,
+        anyToken     : _combinators.anyToken,
+        notFollowedBy: _combinators.notFollowedBy,
+        eof          : _combinators.eof,
+        manyTill     : _combinators.manyTill
     });
 };
