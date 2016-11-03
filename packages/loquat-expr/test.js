56ab25de5dcc4f7d1e4f8e44986027cd9e21c85a
Susisu <susisu2413@yahoo.co.jp>
2016-11-03 12:24:57 +0900

    [expr] Fix requiring wrong package

--- packages/loquat-expr/test.js
+++ packages/loquat-expr/test.js
@@ -7,7 +7,7 @@
 
 global._core       = require("loquat-core")();
 const _prim        = require("loquat-prim")(_core);
-const _combinators = require("loquat-prim")(_core);
+const _combinators = require("loquat-combinators")(_core);
 global._expr       = require("./lib/expr.js")(_core, _prim, _combinators);
 
 require("./test/expr.js");
