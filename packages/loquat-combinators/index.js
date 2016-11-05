a1910d189861bb30234eddc34525aca8d17306bb
Susisu <susisu2413@yahoo.co.jp>
2016-11-05 13:08:35 +0900

    [combinators] Enable more general option

--- packages/loquat-combinators/index.js
+++ packages/loquat-combinators/index.js
@@ -5,11 +5,15 @@
 
 "use strict";
 
-module.exports = (_core, sugar) => {
+module.exports = (_core, opts) => {
+    if (opts === undefined) {
+        opts = {};
+    }
+
     const _prim        = require("loquat-prim")(_core);
     const _combinators = require("./lib/combinators.js")(_core, _prim);
 
-    if (sugar) {
+    if (opts.sugar) {
         const _sugar = require("./lib/sugar.js")(_core, _prim, _combinators);
         _core.extendParser(_sugar);
     }
