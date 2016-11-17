8eb8db8a91000fffb3d3b7d418c9c13aabdb5d6f
Susisu <susisu2413@yahoo.co.jp>
2016-11-17 11:21:33 +0900

    :zap: Export as a function and add options

--- index.js
+++ index.js
@@ -5,48 +5,56 @@
 
 "use strict";
 
-const _core = require("loquat-core")();
-
-const _loquat = Object.assign({}, _core);
-
-Object.defineProperties(_loquat, {
-    "exts": {
-        writable    : false,
-        configurable: false,
-        enumerable  : true,
-        value       : {}
-    },
-    "use": {
-        writable    : false,
-        configurable: false,
-        enumerable  : true,
-        value       : use
-    }
-});
-
-/**
- * @param {Function} ext
- * @param {Object} [opts = {}]
- * @returns {undefined}
- */
-function use(ext, opts) {
+module.exports = opts => {
     if (opts === undefined) {
         opts = {};
     }
-    const _ext = ext(_core, opts.opts);
-    if (!opts.qualified) {
-        Object.assign(_loquat, _ext);
-    }
-    if (opts.as !== undefined) {
-        _loquat.exts[opts.as] = _ext;
-    }
-}
 
-use(require("loquat-prim"),        { as: "prim",        opts: { sugar: true } });
-use(require("loquat-char"),        { as: "char",        opts: { sugar: true } });
-use(require("loquat-combinators"), { as: "combinators", opts: { sugar: true } });
-use(require("loquat-monad"),       { as: "monad",       opts: { sugar: true } });
-use(require("loquat-expr"),        { as: "expr",        opts: {} });
-use(require("loquat-qo"),          { as: "qo",          opts: {} });
+    const _core = require("loquat-core")();
 
-module.exports = _loquat;
+    const _loquat = Object.assign({}, _core);
+
+    Object.defineProperties(_loquat, {
+        "exts": {
+            writable    : false,
+            configurable: false,
+            enumerable  : true,
+            value       : {}
+        },
+        "use": {
+            writable    : false,
+            configurable: false,
+            enumerable  : true,
+            value       : use
+        }
+    });
+
+    /**
+     * @param {Function} ext
+     * @param {Object} [opts = {}]
+     * @returns {undefined}
+     */
+    function use(ext, opts) {
+        if (opts === undefined) {
+            opts = {};
+        }
+        const _ext = ext(_core, opts.opts);
+        if (!opts.qualified) {
+            Object.assign(_loquat, _ext);
+        }
+        if (opts.as !== undefined) {
+            _loquat.exts[opts.as] = _ext;
+        }
+    }
+
+    if (!opts.noUsingDefaults) {
+        use(require("loquat-prim"),        { as: "prim",        opts: { sugar: true } });
+        use(require("loquat-char"),        { as: "char",        opts: { sugar: true } });
+        use(require("loquat-combinators"), { as: "combinators", opts: { sugar: true } });
+        use(require("loquat-monad"),       { as: "monad",       opts: { sugar: true } });
+        use(require("loquat-expr"),        { as: "expr",        opts: {} });
+        use(require("loquat-qo"),          { as: "qo",          opts: {} });
+    }
+
+    return _loquat;
+};
