86dde0e460fd11bca5c2a2115b53a323ba2b90b7
Susisu <susisu2413@yahoo.co.jp>
2016-11-19 18:57:26 +0900

    [token] Add index

---
+++ packages/loquat-token/index.js
@@ -0,0 +1,19 @@
+/*
+ * loquat-token
+ * copyright (c) 2016 Susisu
+ */
+
+"use strict";
+
+module.exports = _core => {
+    const _prim        = require("loquat-prim")(_core);
+    const _char        = require("loquat-char")(_core);
+    const _combinators = require("loquat-combinators")(_core);
+    const _language    = require("./lib/language.js")();
+    const _token       = require("./lib/token.js")(_core, _prim, _char, _combinators);
+
+    return Object.freeze({
+        LanguageDef    : _language.LanguageDef,
+        makeTokenParser: _token.makeTokenParser
+    });
+};
