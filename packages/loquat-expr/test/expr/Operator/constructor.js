bf8f2ef57f1ecee5057dee78d90a3493330903ae
Susisu <susisu2413@yahoo.co.jp>
2017-01-07 10:33:04 +0900

    [expr] Prefer `const`

--- packages/loquat-expr/test/expr/Operator/constructor.js
+++ packages/loquat-expr/test/expr/Operator/constructor.js
@@ -18,23 +18,23 @@ describe("constructor(type, parser, assoc)", () => {
     it("should create a new `Operator' instance", () => {
         // infix operator
         {
-            let parser = new Parser(() => {});
-            let op = new Operator(OperatorType.INFIX, parser, OperatorAssoc.NONE);
+            const parser = new Parser(() => {});
+            const op = new Operator(OperatorType.INFIX, parser, OperatorAssoc.NONE);
             expect(op.type).to.equal(OperatorType.INFIX);
             expect(op.parser).to.equal(parser);
             expect(op.assoc).to.equal(OperatorAssoc.NONE);
         }
         // prefix operator
         {
-            let parser = new Parser(() => {});
-            let op = new Operator(OperatorType.PREFIX, parser);
+            const parser = new Parser(() => {});
+            const op = new Operator(OperatorType.PREFIX, parser);
             expect(op.type).to.equal(OperatorType.PREFIX);
             expect(op.parser).to.equal(parser);
         }
         // postfix operator
         {
-            let parser = new Parser(() => {});
-            let op = new Operator(OperatorType.POSTFIX, parser);
+            const parser = new Parser(() => {});
+            const op = new Operator(OperatorType.POSTFIX, parser);
             expect(op.type).to.equal(OperatorType.POSTFIX);
             expect(op.parser).to.equal(parser);
         }
