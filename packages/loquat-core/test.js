d2f3a7ac5be654b1b3aa6ef71ee4e3398546d9a1
Susisu <susisu2413@yahoo.co.jp>
2019-03-21 20:49:44 +0900

    test(core): use loquat-test

--- packages/loquat-core/test.js
+++ packages/loquat-core/test.js
@@ -1,12 +1,17 @@
 "use strict";
 
+const chai = require("chai");
+
 const _utils  = require("./lib/utils")();
 const _pos    = require("./lib/pos")();
 const _error  = require("./lib/error")({ _pos });
 const _stream = require("./lib/stream")({ _utils });
 const _parser = require("./lib/parser")({ _pos, _error });
+const _core   = require("./lib/core")({ _utils, _pos, _error, _stream, _parser });
 
 Object.assign(global, { _utils, _pos, _error, _stream, _parser });
+// assuming loquat-test is updated in sync with loquat-core
+chai.use(require("../loquat-test")(_core));
 
 require("./test/utils");
 require("./test/pos");
