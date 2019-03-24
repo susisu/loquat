8b34fa105275f022d9ac9eda4564241fe6b819c4
Susisu <susisu2413@yahoo.co.jp>
2019-03-24 16:39:59 +0900

    test(prim): use loquat-test

--- packages/loquat-prim/test.js
+++ packages/loquat-prim/test.js
@@ -1,12 +1,15 @@
-/*
- * loquat-prim test
- */
-
 "use strict";
 
-global._core  = require("loquat-core")();
-global._prim  = require("./lib/prim.js")(_core);
-global._sugar = require("./lib/sugar.js")(_core, _prim);
+const chai = require("chai");
+
+const _core  = require("loquat-core")();
+const _prim  = require("./lib/prim.js")(_core);
+const _sugar = require("./lib/sugar.js")(_core, _prim);
+
+const _test = require("loquat-test")(_core);
+
+Object.assign(global, { _core, _prim, _sugar, _test });
+chai.use(_test.plugin);
 
 require("./test/prim.js");
 require("./test/sugar.js");
