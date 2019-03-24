51c49d9cbd4086d0ba8568bf063284a2d3057d1f
Susisu <susisu2413@yahoo.co.jp>
2019-03-24 20:24:43 +0900

    feat(prim): update `then`

--- packages/loquat-prim/lib/prim.js
+++ packages/loquat-prim/lib/prim.js
@@ -153,26 +153,30 @@ module.exports = _core => {
   }
 
   /**
-   * @function module:prim.then
-   * @static
-   * @param {AbstractParser} parserA
-   * @param {AbstractParser} parserB
-   * @returns {AbstracParser}
+   * then: [S, U, A, B](parserA: Parser[S, U, A], parserB: Parser[S, U, B]): Parser[S, U, B]
    */
   function then(parserA, parserB) {
     return new StrictParser(state => {
       const resA = parserA.run(state);
       if (resA.success) {
         const resB = parserB.run(resA.state);
-        return resB.consumed
-          ? resB
-          : new Result(
-            resA.consumed,
-            resB.success,
-            ParseError.merge(resA.err, resB.err),
-            resB.val,
-            resB.state
-          );
+        if (resB.success) {
+          return resB.consumed
+            ? resB
+            : Result.succ(
+              resA.consumed,
+              ParseError.merge(resA.err, resB.err),
+              resB.val,
+              resB.state
+            );
+        } else {
+          return resB.consumed
+            ? resB
+            : Result.fail(
+              resA.consumed,
+              ParseError.merge(resA.err, resB.err)
+            );
+        }
       } else {
         return resA;
       }
