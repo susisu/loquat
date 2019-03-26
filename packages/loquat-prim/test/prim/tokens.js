eecc6462479e4327ae05801590e581794151664d
Susisu <susisu2413@yahoo.co.jp>
2019-03-26 22:52:25 +0900

    test(prim): improve test for `tokens`

--- packages/loquat-prim/test/prim/tokens.js
+++ packages/loquat-prim/test/prim/tokens.js
@@ -79,18 +79,15 @@ describe("tokens", () => {
       const parser = generateParser(expectTokens);
       expect(parser).to.be.a.parser;
       const res = parser.run(initState);
-      expect(res).to.be.an.equalResultTo(
-        Result.efail(
-          new StrictParseError(
-            initPos,
-            [
-              ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, "C"),
-              ErrorMessage.create(ErrorMessageType.EXPECT, "AB"),
-            ]
-          )
-        ),
-        chai.util.eql
-      );
+      expect(res).to.be.an.equalResultTo(Result.efail(
+        new StrictParseError(
+          initPos,
+          [
+            ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, "C"),
+            ErrorMessage.create(ErrorMessageType.EXPECT, "AB"),
+          ]
+        )
+      ));
     }
     // expect many, partially wrong input
     {
@@ -99,18 +96,15 @@ describe("tokens", () => {
       const parser = generateParser(expectTokens);
       expect(parser).to.be.a.parser;
       const res = parser.run(initState);
-      expect(res).to.be.an.equalResultTo(
-        Result.cfail(
-          new StrictParseError(
-            initPos,
-            [
-              ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, "C"),
-              ErrorMessage.create(ErrorMessageType.EXPECT, "AB"),
-            ]
-          )
-        ),
-        chai.util.eql
-      );
+      expect(res).to.be.an.equalResultTo(Result.cfail(
+        new StrictParseError(
+          initPos,
+          [
+            ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, "C"),
+            ErrorMessage.create(ErrorMessageType.EXPECT, "AB"),
+          ]
+        )
+      ));
     }
     // expect many, no input
     {
@@ -119,18 +113,15 @@ describe("tokens", () => {
       const parser = generateParser(expectTokens);
       expect(parser).to.be.a.parser;
       const res = parser.run(initState);
-      expect(res).to.be.an.equalResultTo(
-        Result.efail(
-          new StrictParseError(
-            initPos,
-            [
-              ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, ""),
-              ErrorMessage.create(ErrorMessageType.EXPECT, "AB"),
-            ]
-          )
-        ),
-        chai.util.eql
-      );
+      expect(res).to.be.an.equalResultTo(Result.efail(
+        new StrictParseError(
+          initPos,
+          [
+            ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, ""),
+            ErrorMessage.create(ErrorMessageType.EXPECT, "AB"),
+          ]
+        )
+      ));
     }
     // expect many, short input
     {
@@ -139,18 +130,15 @@ describe("tokens", () => {
       const parser = generateParser(expectTokens);
       expect(parser).to.be.a.parser;
       const res = parser.run(initState);
-      expect(res).to.be.an.equalResultTo(
-        Result.cfail(
-          new StrictParseError(
-            initPos,
-            [
-              ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, ""),
-              ErrorMessage.create(ErrorMessageType.EXPECT, "AB"),
-            ]
-          )
-        ),
-        chai.util.eql
-      );
+      expect(res).to.be.an.equalResultTo(Result.cfail(
+        new StrictParseError(
+          initPos,
+          [
+            ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, ""),
+            ErrorMessage.create(ErrorMessageType.EXPECT, "AB"),
+          ]
+        )
+      ));
     }
   });
 
@@ -163,7 +151,7 @@ describe("tokens", () => {
         new SourcePos("main", 0, 1, 1),
         "none"
       );
-      const expectTokens = ["\uD83C\uDF63", "A"];
+      const expectTokens = ["\uD83C", "\uDF63", "A"];
       const parser = tokens(
         expectTokens,
         (x, y) => x === y,
@@ -172,19 +160,20 @@ describe("tokens", () => {
           expect(pos).to.be.an.equalPositionTo(initState.pos);
           expect(tokens).to.deep.equal(expectTokens);
           expect(config).to.be.an.equalConfigTo(initState.config);
-          return new SourcePos("main", 2, 1, 3);
+          return new SourcePos("main", 3, 1, 4);
         }
       );
       expect(parser).to.be.a.parser;
       const res = parser.run(initState);
       expect(res).to.be.an.equalResultTo(
-        Result.efail(
-          new StrictParseError(
-            new SourcePos("main", 0, 1, 1),
-            [
-              ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, "\uD83C"),
-              ErrorMessage.create(ErrorMessageType.EXPECT, "\uD83C\uDF63A"),
-            ]
+        Result.csucc(
+          ParseError.unknown(new SourcePos("main", 3, 1, 4)),
+          ["\uD83C", "\uDF63", "A"],
+          new State(
+            initState.config,
+            "BC",
+            new SourcePos("main", 3, 1, 4),
+            "none"
           )
         ),
         chai.util.eql
@@ -207,19 +196,19 @@ describe("tokens", () => {
           expect(pos).to.be.an.equalPositionTo(initState.pos);
           expect(tokens).to.deep.equal(expectTokens);
           expect(config).to.be.an.equalConfigTo(initState.config);
-          return new SourcePos("main", 2, 1, 3);
+          return new SourcePos("main", 3, 1, 3);
         }
       );
       expect(parser).to.be.a.parser;
       const res = parser.run(initState);
       expect(res).to.be.an.equalResultTo(
         Result.csucc(
-          ParseError.unknown(new SourcePos("main", 2, 1, 3)),
+          ParseError.unknown(new SourcePos("main", 3, 1, 3)),
           ["\uD83C\uDF63", "A"],
           new State(
             initState.config,
             "BC",
-            new SourcePos("main", 2, 1, 3),
+            new SourcePos("main", 3, 1, 3),
             "none"
           )
         ),
