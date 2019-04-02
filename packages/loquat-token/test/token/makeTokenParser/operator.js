91f06eac8d9fc076e36c09065ab89f32c1c66989
Susisu <susisu2413@yahoo.co.jp>
2019-04-03 00:40:22 +0900

    feat(token): update `operator` and `reservedOp`

--- packages/loquat-token/test/token/makeTokenParser/operator.js
+++ packages/loquat-token/test/token/makeTokenParser/operator.js
@@ -18,49 +18,49 @@ const {
 const { LanguageDef } = _language;
 const { makeTokenParser } = _token;
 
-function genCharParser(re) {
-  return new StrictParser(state => {
-    const unconsed = uncons(state.input, state.config.unicode);
-    if (unconsed.empty) {
-      return Result.efail(
-        new StrictParseError(
-          state.pos,
-          [ErrorMessage.create(ErrorMessageType.MESSAGE, "e")]
-        )
-      );
-    } else {
-      if (re.test(unconsed.head)) {
-        const newPos = state.pos.addChar(unconsed.head, state.config.tabWidth);
-        return Result.csucc(
-          new StrictParseError(
-            newPos,
-            [ErrorMessage.create(ErrorMessageType.MESSAGE, "C")]
-          ),
-          unconsed.head,
-          new State(
-            state.config,
-            unconsed.tail,
-            newPos,
-            state.userState
-          )
-        );
-      } else {
+describe("operator", () => {
+  function genCharParser(re) {
+    return new StrictParser(state => {
+      const unconsed = uncons(state.input, state.config);
+      if (unconsed.empty) {
         return Result.efail(
           new StrictParseError(
             state.pos,
             [ErrorMessage.create(ErrorMessageType.MESSAGE, "e")]
           )
         );
+      } else {
+        if (re.test(unconsed.head)) {
+          const newPos = state.pos.addChar(unconsed.head, state.config.tabWidth);
+          return Result.csucc(
+            new StrictParseError(
+              newPos,
+              [ErrorMessage.create(ErrorMessageType.MESSAGE, "C")]
+            ),
+            unconsed.head,
+            new State(
+              state.config,
+              unconsed.tail,
+              newPos,
+              state.userState
+            )
+          );
+        } else {
+          return Result.efail(
+            new StrictParseError(
+              state.pos,
+              [ErrorMessage.create(ErrorMessageType.MESSAGE, "e")]
+            )
+          );
+        }
       }
-    }
-  });
-}
+    });
+  }
 
-const opStart = genCharParser(/[+\-*/%=<>]/);
-const opLetter = genCharParser(/[+\-*/%=<>:]/);
+  const opStart = genCharParser(/[+\-*/%=<>]/);
+  const opLetter = genCharParser(/[+\-*/%=<>:]/);
 
-describe(".operator", () => {
-  it("should parse an operator", () => {
+  it("should be a parser that parses an operator", () => {
     const def = new LanguageDef({
       opStart : opStart,
       opLetter: opLetter,
@@ -72,54 +72,48 @@ describe(".operator", () => {
       const initState = new State(
         new Config({ tabWidth: 8 }),
         "+: XYZ",
-        new SourcePos("foobar", 1, 1),
+        new SourcePos("main", 0, 1, 1),
         "none"
       );
       const res = operator.run(initState);
-      expect(Result.equal(
-        res,
-        Result.csucc(
-          new StrictParseError(
-            new SourcePos("foobar", 1, 4),
-            [
-              ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, show("X")),
-              ErrorMessage.create(ErrorMessageType.EXPECT, ""),
-            ]
-          ),
-          "+:",
-          new State(
-            new Config({ tabWidth: 8 }),
-            "XYZ",
-            new SourcePos("foobar", 1, 4),
-            "none"
-          )
+      expect(res).to.be.an.equalResultTo(Result.csucc(
+        new StrictParseError(
+          new SourcePos("main", 3, 1, 4),
+          [
+            ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, show("X")),
+            ErrorMessage.create(ErrorMessageType.EXPECT, ""),
+          ]
+        ),
+        "+:",
+        new State(
+          new Config({ tabWidth: 8 }),
+          "XYZ",
+          new SourcePos("main", 3, 1, 4),
+          "none"
         )
-      )).to.be.true;
+      ));
     }
     {
       const initState = new State(
         new Config({ tabWidth: 8 }),
         ":+ XYZ",
-        new SourcePos("foobar", 1, 1),
+        new SourcePos("main", 0, 1, 1),
         "none"
       );
       const res = operator.run(initState);
-      expect(Result.equal(
-        res,
-        Result.efail(
-          new StrictParseError(
-            new SourcePos("foobar", 1, 1),
-            [
-              ErrorMessage.create(ErrorMessageType.MESSAGE, "e"),
-              ErrorMessage.create(ErrorMessageType.EXPECT, "operator"),
-            ]
-          )
+      expect(res).to.be.an.equalResultTo(Result.efail(
+        new StrictParseError(
+          new SourcePos("main", 0, 1, 1),
+          [
+            ErrorMessage.create(ErrorMessageType.MESSAGE, "e"),
+            ErrorMessage.create(ErrorMessageType.EXPECT, "operator"),
+          ]
         )
-      )).to.be.true;
+      ));
     }
   });
 
-  it("should not accept reserved operator", () => {
+  it("should not accept any reserved operator", () => {
     {
       const def = new LanguageDef({
         opStart    : opStart,
@@ -132,29 +126,26 @@ describe(".operator", () => {
       const initState = new State(
         new Config({ tabWidth: 8 }),
         "-> XYZ",
-        new SourcePos("foobar", 1, 1),
+        new SourcePos("main", 0, 1, 1),
         "none"
       );
       const res = operator.run(initState);
-      expect(Result.equal(
-        res,
-        Result.csucc(
-          new StrictParseError(
-            new SourcePos("foobar", 1, 4),
-            [
-              ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, show("X")),
-              ErrorMessage.create(ErrorMessageType.EXPECT, ""),
-            ]
-          ),
-          "->",
-          new State(
-            new Config({ tabWidth: 8 }),
-            "XYZ",
-            new SourcePos("foobar", 1, 4),
-            "none"
-          )
+      expect(res).to.be.an.equalResultTo(Result.csucc(
+        new StrictParseError(
+          new SourcePos("main", 3, 1, 4),
+          [
+            ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, show("X")),
+            ErrorMessage.create(ErrorMessageType.EXPECT, ""),
+          ]
+        ),
+        "->",
+        new State(
+          new Config({ tabWidth: 8 }),
+          "XYZ",
+          new SourcePos("main", 3, 1, 4),
+          "none"
         )
-      )).to.be.true;
+      ));
     }
     {
       const def = new LanguageDef({
@@ -168,22 +159,19 @@ describe(".operator", () => {
       const initState = new State(
         new Config({ tabWidth: 8 }),
         "-> XYZ",
-        new SourcePos("foobar", 1, 1),
+        new SourcePos("main", 0, 1, 1),
         "none"
       );
       const res = operator.run(initState);
-      expect(Result.equal(
-        res,
-        Result.efail(
-          new StrictParseError(
-            new SourcePos("foobar", 1, 3),
-            [
-              ErrorMessage.create(ErrorMessageType.MESSAGE, "e"),
-              ErrorMessage.create(ErrorMessageType.UNEXPECT, "reserved operator " + show("->")),
-            ]
-          )
+      expect(res).to.be.an.equalResultTo(Result.efail(
+        new StrictParseError(
+          new SourcePos("main", 2, 1, 3),
+          [
+            ErrorMessage.create(ErrorMessageType.MESSAGE, "e"),
+            ErrorMessage.create(ErrorMessageType.UNEXPECT, "reserved operator " + show("->")),
+          ]
         )
-      )).to.be.true;
+      ));
     }
   });
 });
--- packages/loquat-token/test/token/makeTokenParser/reservedOp.js
+++ packages/loquat-token/test/token/makeTokenParser/reservedOp.js
@@ -18,56 +18,55 @@ const {
 const { LanguageDef } = _language;
 const { makeTokenParser } = _token;
 
-function genCharParser(re) {
-  return new StrictParser(state => {
-    const unconsed = uncons(state.input, state.config.unicode);
-    if (unconsed.empty) {
-      return Result.efail(
-        new StrictParseError(
-          state.pos,
-          [ErrorMessage.create(ErrorMessageType.MESSAGE, "e")]
-        )
-      );
-    } else {
-      if (re.test(unconsed.head)) {
-        const newPos = state.pos.addChar(unconsed.head, state.config.tabWidth);
-        return Result.csucc(
-          new StrictParseError(
-            newPos,
-            [ErrorMessage.create(ErrorMessageType.MESSAGE, "C")]
-          ),
-          unconsed.head,
-          new State(
-            state.config,
-            unconsed.tail,
-            newPos,
-            state.userState
-          )
-        );
-      } else {
+describe("reservedOp", () => {
+  function genCharParser(re) {
+    return new StrictParser(state => {
+      const unconsed = uncons(state.input, state.config.unicode);
+      if (unconsed.empty) {
         return Result.efail(
           new StrictParseError(
             state.pos,
             [ErrorMessage.create(ErrorMessageType.MESSAGE, "e")]
           )
         );
+      } else {
+        if (re.test(unconsed.head)) {
+          const newPos = state.pos.addChar(unconsed.head, state.config.tabWidth);
+          return Result.csucc(
+            new StrictParseError(
+              newPos,
+              [ErrorMessage.create(ErrorMessageType.MESSAGE, "C")]
+            ),
+            unconsed.head,
+            new State(
+              state.config,
+              unconsed.tail,
+              newPos,
+              state.userState
+            )
+          );
+        } else {
+          return Result.efail(
+            new StrictParseError(
+              state.pos,
+              [ErrorMessage.create(ErrorMessageType.MESSAGE, "e")]
+            )
+          );
+        }
       }
-    }
-  });
-}
+    });
+  }
 
-const opStart = genCharParser(/[+\-*/%=<>]/);
-const opLetter = genCharParser(/[+\-*/%=<>:]/);
+  const opStart = genCharParser(/[+\-*/%=<>]/);
+  const opLetter = genCharParser(/[+\-*/%=<>:]/);
 
-describe(".reservedOp(name)", () => {
-  const def = new LanguageDef({
-    opStart : opStart,
-    opLetter: opLetter,
-  });
-  const tp = makeTokenParser(def);
-  const reservedOp = tp.reservedOp;
-
-  it("should parse a reserved operator `name'", () => {
+  it("should create a parser that parses the given reserved operator", () => {
+    const def = new LanguageDef({
+      opStart : opStart,
+      opLetter: opLetter,
+    });
+    const tp = makeTokenParser(def);
+    const reservedOp = tp.reservedOp;
     expect(reservedOp).to.be.a("function");
     const parser = reservedOp("->");
     expect(parser).to.be.a.parser;
@@ -75,93 +74,81 @@ describe(".reservedOp(name)", () => {
       const initState = new State(
         new Config({ tabWidth: 8, unicode: false }),
         "-> XYZ",
-        new SourcePos("foobar", 1, 1),
+        new SourcePos("main", 0, 1, 1),
         "none"
       );
       const res = parser.run(initState);
-      expect(Result.equal(
-        res,
-        Result.csucc(
-          new StrictParseError(
-            new SourcePos("foobar", 1, 4),
-            [
-              ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, show("X")),
-              ErrorMessage.create(ErrorMessageType.EXPECT, ""),
-            ]
-          ),
-          undefined,
-          new State(
-            new Config({ tabWidth: 8, unicode: false }),
-            "XYZ",
-            new SourcePos("foobar", 1, 4),
-            "none"
-          )
+      expect(res).to.be.an.equalResultTo(Result.csucc(
+        new StrictParseError(
+          new SourcePos("main", 3, 1, 4),
+          [
+            ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, show("X")),
+            ErrorMessage.create(ErrorMessageType.EXPECT, ""),
+          ]
+        ),
+        undefined,
+        new State(
+          new Config({ tabWidth: 8, unicode: false }),
+          "XYZ",
+          new SourcePos("main", 3, 1, 4),
+          "none"
         )
-      )).to.be.true;
+      ));
     }
     {
       const initState = new State(
         new Config({ tabWidth: 8, unicode: false }),
         "- XYZ",
-        new SourcePos("foobar", 1, 1),
+        new SourcePos("main", 0, 1, 1),
         "none"
       );
       const res = parser.run(initState);
-      expect(Result.equal(
-        res,
-        Result.efail(
-          new StrictParseError(
-            new SourcePos("foobar", 1, 1),
-            [
-              ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, show(" ")),
-              ErrorMessage.create(ErrorMessageType.EXPECT, show("->")),
-            ]
-          )
+      expect(res).to.be.an.equalResultTo(Result.efail(
+        new StrictParseError(
+          new SourcePos("main", 0, 1, 1),
+          [
+            ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, show(" ")),
+            ErrorMessage.create(ErrorMessageType.EXPECT, show("->")),
+          ]
         )
-      )).to.be.true;
+      ));
     }
     {
       const initState = new State(
         new Config({ tabWidth: 8, unicode: false }),
         "->+",
-        new SourcePos("foobar", 1, 1),
+        new SourcePos("main", 0, 1, 1),
         "none"
       );
       const res = parser.run(initState);
-      expect(Result.equal(
-        res,
-        Result.efail(
-          new StrictParseError(
-            new SourcePos("foobar", 1, 4),
-            [
-              ErrorMessage.create(ErrorMessageType.MESSAGE, "C"),
-              ErrorMessage.create(ErrorMessageType.UNEXPECT, show("+")),
-              ErrorMessage.create(ErrorMessageType.EXPECT, "end of " + show("->")),
-            ]
-          )
+      expect(res).to.be.an.equalResultTo(Result.efail(
+        new StrictParseError(
+          new SourcePos("main", 3, 1, 4),
+          [
+            ErrorMessage.create(ErrorMessageType.MESSAGE, "C"),
+            ErrorMessage.create(ErrorMessageType.UNEXPECT, show("+")),
+            ErrorMessage.create(ErrorMessageType.EXPECT, "end of " + show("->")),
+          ]
         )
-      )).to.be.true;
+      ));
     }
     {
       const initState = new State(
         new Config({ tabWidth: 8, unicode: false }),
         "XYZ",
-        new SourcePos("foobar", 1, 1),
+        new SourcePos("main", 0, 1, 1),
         "none"
       );
       const res = parser.run(initState);
-      expect(Result.equal(
-        res,
-        Result.efail(
-          new StrictParseError(
-            new SourcePos("foobar", 1, 1),
-            [
-              ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, show("X")),
-              ErrorMessage.create(ErrorMessageType.EXPECT, show("->")),
-            ]
-          )
+      expect(res).to.be.an.equalResultTo(Result.efail(
+        new StrictParseError(
+          new SourcePos("main", 0, 1, 1),
+          [
+            ErrorMessage.create(ErrorMessageType.SYSTEM_UNEXPECT, show("X")),
+            ErrorMessage.create(ErrorMessageType.EXPECT, show("->")),
+          ]
         )
-      )).to.be.true;
+      ));
     }
   });
 });
