10e68eac0b416a9214b5c8f14455e993d8f7bd8f
Susisu <susisu2413@yahoo.co.jp>
2019-04-04 23:26:50 +0900

    test(core): remove dependency to loquat-test

--- packages/loquat-core/test.js
+++ packages/loquat-core/test.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const chai = require("chai");
-
 const _utils  = require("./lib/utils")();
 const _pos    = require("./lib/pos")();
 const _error  = require("./lib/error")({ _pos });
@@ -9,10 +7,7 @@ const _parser = require("./lib/parser")({ _pos, _error });
 const _stream = require("./lib/stream")({ _utils });
 const _core   = require("./lib/core")({ _utils, _pos, _error, _parser, _stream });
 
-const _test = require("loquat-test")(_core);
-
-Object.assign(global, { _utils, _pos, _error, _stream, _parser, _core, _test });
-chai.use(_test.plugin);
+Object.assign(global, { _utils, _pos, _error, _stream, _parser, _core });
 
 require("./test/utils");
 require("./test/pos");
