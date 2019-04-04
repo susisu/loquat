76abb7cd9ddeb0c060e4b5c18f6b3f9ab8465ab6
Susisu <susisu2413@yahoo.co.jp>
2019-04-05 00:39:23 +0900

    test(loquat): add tests

--- packages/loquat/test.js
+++ packages/loquat/test.js
@@ -2,8 +2,74 @@
 
 const { expect } = require("chai");
 
-describe("it", () => {
-  it("should work", () => {
-    expect(1 + 1).to.equal(2);
+const _loquat = require("./index");
+
+describe("loquat", () => {
+  it("should yield a new loquat parsers", () => {
+    const lq = _loquat();
+    expect(lq).to.be.an("object");
+    expect(lq.Parser).to.be.a("function");
+    expect(lq.bind).to.be.a("function");
+    expect(lq.Parser.prototype.bind).to.be.a("function");
+  });
+
+  it("should not use default plugins if `noUsingDefaults = true` is specified", () => {
+    const lq = _loquat({ noUsingDefaults: true });
+    expect(lq).to.be.an("object");
+    expect(lq.Parser).to.be.a("function");
+    expect(lq.bind).to.be.undefined;
+    expect(lq.Parser.prototype.bind).to.be.undefined;
+  });
+
+  describe("use", () => {
+    it("should use a plugin", () => {
+      const lq = _loquat();
+      const plugin = (_core, opts) => {
+        expect(_core).to.be.an("object");
+        expect(_core.Parser).to.be.a("function");
+        expect(opts).to.be.undefined;
+        return { foo: "bar" };
+      };
+      lq.use(plugin);
+      expect(lq.foo).to.equal("bar");
+    });
+
+    it("should pass options to a plugin", () => {
+      const lq = _loquat();
+      const plugin = (_core, opts) => {
+        expect(_core).to.be.an("object");
+        expect(_core.Parser).to.be.a("function");
+        expect(opts).to.deep.equal({ answer: 42 });
+        return { foo: "bar" };
+      };
+      lq.use(plugin, { options: { answer: 42 } });
+      expect(lq.foo).to.equal("bar");
+    });
+
+    it("should set the plugin to `extensions` property if name is specified", () => {
+      const lq = _loquat();
+      const plugin = (_core, opts) => {
+        expect(_core).to.be.an("object");
+        expect(_core.Parser).to.be.a("function");
+        expect(opts).to.be.undefined;
+        return { foo: "bar" };
+      };
+      lq.use(plugin, { name: "myplugin" });
+      expect(lq.foo).to.equal("bar");
+      expect(lq.extensions.myplugin).to.deep.equal({ foo: "bar" });
+    });
+
+    it("should not extract properties to the root if `qualified = true` is specified", () => {
+      const lq = _loquat();
+      const plugin = (_core, opts) => {
+        expect(_core).to.be.an("object");
+        expect(_core.Parser).to.be.a("function");
+        expect(opts).to.be.undefined;
+        return { foo: "bar" };
+      };
+      lq.use(plugin, { name: "myplugin", qualified: true });
+      expect(lq.foo).to.be.undefined;
+      expect(lq.extensions.myplugin).to.deep.equal({ foo: "bar" });
+    });
   });
 });
